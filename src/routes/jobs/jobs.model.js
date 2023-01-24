5cdffafa7a88437ed3d33ca1fd9735301ab1a78b
suFi7867 <noorani786.ss@gmail.com>
2023-01-24 15:24:22 +0530

    JOBS COLLECTION CREATED

--- src/routes/jobs/jobs.model.js
+++ src/routes/jobs/jobs.model.js
@@ -2,7 +2,7 @@ const {Schema,model} = require('mongoose')
 
 const JobsSchema = new Schema({
 
-    
+
 name  : { type:String, required:true },
 location :{type:String},
 contract :{type:String},
@@ -12,5 +12,5 @@ jobrole :{type:String},
 
 },{versionKey:false})
 
-const JobsModel = model("jobs",JobsSchema)
-module.exports=JobsModel
+const Jobs = model("jobs",JobsSchema)
+module.exports=Jobs
